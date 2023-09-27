3f7be28c5edb90a612f5f0ea906e6c01a15645e1
Alexey Kozyrev <tcsid4@gmail.com>
2023-09-27 23:09:58 +0300

    готовая ДЗ 8

--- app/src/main/java/com/example/homework8/Contacts.kt
+++ app/src/main/java/com/example/homework8/Contacts.kt
@@ -1,14 +1,11 @@
 package com.example.homework8
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class Contacts: AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.contact_layout)
     }
 }
--- app/src/main/java/com/example/homework8/RegistrationLayout.kt
+++ app/src/main/java/com/example/homework8/RegistrationLayout.kt
@@ -1,13 +1,13 @@
 package com.example.homework8
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class RegistrationLayout: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.registration_layout)
 
 
     }
